60fff99f94628952614f362b8d82396c2040b2cd
Anna Ikoki <ikokianna@gmail.com>
2021-06-17 10:07:54 +0200

    Fix auto-refresh from form tag

--- README.md
+++ README.md
@@ -17,13 +17,27 @@ If a player guesses a secret number, he or she wins it!
 
 ### Must-have features
 
-- A secret number will be generated. To make things fair, the number should be between 1 and (learners in the class)
-- A user can enter a guess (what html element can be useful for this?)
-- When he or she pushes the "play" button, the number will be compared to the secret number
-- Depending on the outcome, an alert box will show:
+- [ ] A secret number will be generated. To make things fair, the number should be between 1 and (learners in the class)
+- [x] A user can enter a guess (what html element can be useful for this?)
+- [ ] When he or she pushes the "play" button, the number will be compared to the secret number
+- [ ] Depending on the outcome, an alert box will show:
   - Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them.
   - Bummer... You guessed (guess) and the secret number was (secret number).
 
 ### Nice-to-have features
 
-When a guess was just one number from the secret number, show another message: "So close, but you just missed it! Are you in a class that started on the thirteenth or what?"
+> Show fun text: When a guess was just one number from the secret number, show another message: "So close, but you just missed it! Are you in a class that started on the thirteenth or what?"
+
+> Limit guessing turns: See if you can guess it in 10 turns or fewer. and show the correct answer in the end
+
+> Give hints: We'll tell you if your guess was too high or too low.
+
+> Color: give color to text depending on whether she is right: green, or wrong: red
+
+> Keep guessing until correct answer
+
+> Show game over and start new game buttons, or clear game-board
+
+> Show user guesses
+
+> [Other guessing games?](https://games4esl.com/guessing-games-for-kids/)
--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 // TODO: Generate (secret) number between 1 and 15(class learners) 
 let getRandomInt = max => Math.floor(Math.random() * max) + 1;
+console.log('start script');
 
 const verouTeam = 15;
 // console.log(getRandomInt(verouTeam)); //important to check
-let secretNumber = getRandomInt(verouTeam);
-console.log(secretNumber); //important to check
+// let secretNumber = getRandomInt(verouTeam);
+// console.log(secretNumber); //important to check
 
 // TODO: Title
 document.getElementById('title').innerHTML = `We have selected a random number between 1 and ${verouTeam}.`
 
-// TODO: Grab guessed number from user after being submitted by button
+// TODO: Grab guessed number from user after being submitted by button, and write in the board
+// Get tags needed for these actions
 let submitBtn = document.getElementById('submitBtn');
 let userGuess = document.getElementById('userGuess');
 let messageBoard = document.getElementById('messageBoard');
+console.log('variable initialized');
 
 submitBtn.addEventListener('click', getMessage);
 
 // TODO: Compare numbers secret vs generated
 let compareValues = () => {
-  let userGuessNumber = userGuess.value;
-  // console.log(userGuessNumber); 
-  return secretNumber == userGuessNumber;
+  let secretNumber = getRandomInt(verouTeam);
+  let guessNumber = userGuess.value;
+  console.log(secretNumber); 
+  return secretNumber == guessNumber;
 }
+console.log('compared values');
+  
 
 // TODO: Generate and show message in an alert
-//  - Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them.
-// - Bummer... You guessed (guess) and the secret number was (secret number).
+// TODO: Fix Error- message disappear soon as I release button, but if I click 3 times, it sticks
+// DONE: Removed form tag around the input and button as it has a default refresh page behavior. 
 
 function getMessage() {
-  compareValues() ? messageBoard.innerHTML = 'yeyy' : messageBoard.innerHTML = 'nooo';
+  console.log('click detected');
+  compareValues() ? messageBoard.innerHTML = `Awesome! Your number was correct. You can be named many things, hungry not being one of them.`
+  : messageBoard.innerHTML = `Bummer... You guessed wrong`;
 }
 
